dccc8c07c6e2d24121260ce17cfb2f0bc5c0aa53
Niel de Wet <nieldw@users.noreply.github.com>
2018-02-19 00:10:54 +0200

    Add IndexListSolutionPrinter

---
+++ knapsack-tools/src/main/java/com/github/nieldw/packer/IndexListSolutionPrinter.java
@@ -0,0 +1,40 @@
+package com.github.nieldw.packer;
+
+import com.github.nieldw.knapsack.Item;
+import com.github.nieldw.knapsack.KnapsackSolution;
+
+import java.util.List;
+
+/**
+ * Prints the indexes of a list of solutions, one per line. Prints a dash if there's no solution.
+ * <p>
+ * For example
+ * 4
+ * -
+ * 2,7
+ * 8,9
+ */
+public class IndexListSolutionPrinter implements SolutionPrinter {
+    @Override
+    public String print(List<KnapsackSolution> solution) {
+        StringBuilder output = new StringBuilder();
+        for (KnapsackSolution knapsackSolution : solution) {
+            StringBuilder line = new StringBuilder();
+            List<Item> items = knapsackSolution.getItems();
+            if (items.isEmpty()) {
+                output.append("-\n");
+                break;
+            }
+
+            for (Item item : items) {
+                if (line.length() == 0) {
+                    line.append(item.getIndex());
+                } else {
+                    line.append(",").append(item.getIndex());
+                }
+            }
+            output.append(line).append("\n");
+        }
+        return output.toString();
+    }
+}
---
+++ knapsack-tools/src/test/java/com/github/nieldw/packer/IndexListSolutionPrinterTest.kt
@@ -0,0 +1,36 @@
+package com.github.nieldw.packer
+
+import com.github.nieldw.knapsack.Item
+import com.github.nieldw.knapsack.KnapsackSolution
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import java.math.BigDecimal.ZERO
+import kotlin.test.expect
+
+internal class IndexListSolutionPrinterTest {
+    private lateinit var solutionPrinter: SolutionPrinter
+
+    @BeforeEach
+    fun `set up printer`() {
+        this.solutionPrinter = IndexListSolutionPrinter()
+    }
+
+    @Test
+    fun `should print empty string for empty solution list`() {
+        expect("", { solutionPrinter.print(emptyList()) })
+    }
+
+    @Test
+    fun `should print solutions correctly`() {
+        expect(
+                "1\n" +
+                        "2,4\n" +
+                        "-\n", {
+            solutionPrinter.print(listOf(
+                    KnapsackSolution(listOf(Item(1, ZERO, ZERO))),
+                    KnapsackSolution(listOf(Item(2, ZERO, ZERO), Item(4, ZERO, ZERO))),
+                    KnapsackSolution(emptyList())
+            ))
+        })
+    }
+}
