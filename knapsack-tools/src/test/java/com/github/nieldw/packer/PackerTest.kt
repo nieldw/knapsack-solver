fb18f1f4f8a211d61ed73da5d69b2458e69fbf24
Niel de Wet <nieldw@users.noreply.github.com>
2018-02-19 01:35:26 +0200

    Add integration test for sample problems

--- knapsack-tools/src/test/java/com/github/nieldw/packer/PackerTest.kt
+++ knapsack-tools/src/test/java/com/github/nieldw/packer/PackerTest.kt
@@ -1,6 +1,7 @@
 package com.github.nieldw.packer
 
 import org.junit.jupiter.api.Test
+import java.nio.file.Files
 import java.nio.file.Paths
 import kotlin.test.assertFailsWith
 import kotlin.test.expect
@@ -19,5 +20,17 @@ internal class PackerTest {
     @Test
     fun `pack should throw an APIException if the file is not found`() {
         assertFailsWith<APIException> { Packer.pack("does not exist") }
+    }
+
+    @Test
+    fun `sample problems`() {
+        val sampleProblemsResource = PackerTest::class.java.getResource("/sampleKnapsackProblems.txt")
+        val sampleProblemsFile = Paths.get(sampleProblemsResource.toURI()).toFile()
+        val sampleSolutionsResource = PackerTest::class.java.getResource("/sampleKnapsackSolutions.txt")
+        val sampleSolutionsPath = Paths.get(sampleSolutionsResource.toURI())
+
+        expect(String(Files.readAllBytes(sampleSolutionsPath)), {
+            Packer.pack(sampleProblemsFile.absolutePath)
+        })
     }
 }
